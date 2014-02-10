fbb491956666e1496bda8ab094065535b393aa92
Mateo Vidal <jvidalba@gmail.com>
2014-02-10 13:38:02 -0500

    creating mgrations for interesados and propietarios

--- db/migrate/20140210163214_create_interesados.rb
+++ db/migrate/20140210163214_create_interesados.rb
@@ -1,7 +1,14 @@
 class CreateInteresados < ActiveRecord::Migration
   def up
+    create_table :interesados do |t|
+      t.string :nombre
+      t.string :telefono
+      t.string :celular
+      t.timestamps
+    end
   end
 
   def down
+    drop_table :interesados
   end
 end
--- db/migrate/20140210163229_create_propietarios.rb
+++ db/migrate/20140210163229_create_propietarios.rb
@@ -1,7 +1,16 @@
 class CreatePropietarios < ActiveRecord::Migration
   def up
+    create_table :propietarios do |t|
+      t.string :telefono
+      t.string :direccion
+      t.string :celular
+      t.string :email
+      t.string :nombre
+      t.timestamps
+    end
   end
 
   def down
+    drop_table :propietarios
   end
 end
