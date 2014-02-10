5c7c30bf224d2b207ea5ecad99fc9482c7860da8
Mateo Vidal <jvidalba@gmail.com>
2014-02-09 23:17:14 -0500

    model in

--- app/models/inmueble.rb
+++ app/models/inmueble.rb
@@ -1,5 +1,5 @@
 class Inmueble < ActiveRecord::Base
   # attr_accessible :title, :body
-
+  attr_accessible
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,52 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140113000842) do
+ActiveRecord::Schema.define(:version => 20140119194103) do
+
+  create_table "inmuebles", :force => true do |t|
+    t.integer  "codigo_inmueble"
+    t.integer  "clase_inmueble"
+    t.integer  "ciudad_id"
+    t.string   "zona"
+    t.integer  "tipo_negocio"
+    t.string   "direccion"
+    t.decimal  "area_disponible",         :precision => 15, :scale => 2
+    t.string   "area_disponible_desc"
+    t.decimal  "valor_mt2",               :precision => 15, :scale => 2
+    t.decimal  "valor_total",             :precision => 15, :scale => 2
+    t.string   "planos"
+    t.decimal  "valor_admin",             :precision => 15, :scale => 2
+    t.decimal  "predial",                 :precision => 15, :scale => 2
+    t.decimal  "frente",                  :precision => 15, :scale => 2
+    t.decimal  "fondo",                   :precision => 15, :scale => 2
+    t.string   "otras_caracteristicas"
+    t.string   "entorno"
+    t.string   "info_adicional"
+    t.date     "fecha_disponibilidad"
+    t.date     "fecha_salida_inventario"
+    t.integer  "anio_construccion"
+    t.string   "normas_de_uso"
+    t.decimal  "indice_ocupacion",        :precision => 15, :scale => 2
+    t.decimal  "indice_construccion",     :precision => 15, :scale => 2
+    t.integer  "nro_pisos"
+    t.integer  "parqueadores"
+    t.decimal  "densidad",                :precision => 15, :scale => 2
+    t.string   "nombre_agente"
+    t.string   "quien_refiere"
+    t.string   "telefono"
+    t.string   "celular"
+    t.string   "email"
+    t.integer  "propietario_id"
+    t.decimal  "porcentaje_comision",     :precision => 15, :scale => 2
+    t.decimal  "valor_inmueble",          :precision => 15, :scale => 2
+    t.decimal  "altura",                  :precision => 15, :scale => 2
+    t.integer  "interesado_id"
+    t.date     "fecha_captado"
+    t.string   "transporte"
+    t.integer  "nro_pisos_permitidos"
+    t.datetime "created_at",                                             :null => false
+    t.datetime "updated_at",                                             :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
