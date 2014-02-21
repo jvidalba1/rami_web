eb8900fd01120a0f195ba88790dbbd13c902cd89
Mateo Vidal <jvidalba@gmail.com>
2014-02-20 22:50:18 -0500

    formulario de inmuebles

--- app/helpers/inmuebles_helper.rb
+++ app/helpers/inmuebles_helper.rb
@@ -1,2 +1,19 @@
 module InmueblesHelper
+
+  def select_clase_inmueble
+    [
+        ["Bodega",  1],
+        ["Local",   2],
+        ["Oficina", 3],
+        ["Casa",    4],
+        ["Lote",    5]
+    ]
+  end
+
+  def select_tipo_negocio
+    [
+        ["Arriendo",  1],
+        ["Compra",    2]
+    ]
+  end
 end
