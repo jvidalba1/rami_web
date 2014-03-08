acf905ffffb01538afefb08cff0cef37aa7fd5fd
Mateo Vidal <jvidalba@gmail.com>
2014-03-07 23:34:28 -0500

    pdf custom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,14 @@ class ApplicationController < ActionController::Base
         :orientation  => 'Landscape',
         :exe_path => '/usr/local/bin/wkhtmltopdf',
         :layout => "pdf.html",
+        #:no_background => false,
+        #:image => true,
+        #:header => {
+        #              :html => {
+        #                  :template=> 'layouts/pdf_header.html.erb',
+        #                          :layout   => "pdf_header.html",
+        #                        }
+        #}
         #:footer => {
         #    :left => "Visionamos - SPS",
         #    #:left => "#{Entidad.find(@current_user.entidad).nombre}",
