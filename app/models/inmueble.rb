af0250eeea088a2692c10b9aaba11d4f971f112f
Mateo Vidal <jvidalba@gmail.com>
2014-03-22 20:24:00 -0500

    first version of inmueble report

--- app/models/inmueble.rb
+++ app/models/inmueble.rb
@@ -10,17 +10,54 @@ class Inmueble < ActiveRecord::Base
 
   accepts_nested_attributes_for :propietarios
   
-  attr_accessible :id, :codigo_inmueble, :clase_inmueble, :ciudad_id, :zona, :tipo_negocio,
-                  :direccion, :area_disponible, :area_disponible_desc, :valor_mt2, 
-                  :valor_total, :planos, :valor_admin, :predial, :frente, :fondo, 
-                  :otras_caracteristicas, :entorno, :info_adicional, :fecha_disponibilidad, 
-                  :fecha_salida_inventario, :anio_construccion, :normas_de_uso, :indice_ocupacion,
-                  :indice_construccion, :nro_pisos, :parqueadores, :densidad, :nombre_agente,
-                  :quien_refiere, :telefono, :celular, :email, :propietario_id, :porcentaje_comision,
-                  :valor_inmueble, :altura, :fecha_captado, :transporte, 
-                  :nro_pisos_permitidos, :created_at, :updated_at,
-                  :nombre_interesado, :telefono_interesado, :celular_interesado, :email_interesado,
-                  :observaciones_interesado, :documentos_attributes
+  attr_accessible :id,
+                  :codigo_inmueble,                     # ya
+                  :clase_inmueble,                      # ya
+                  :ciudad_id,                           # ya
+                  :zona,                                # ya
+                  :tipo_negocio,                        # ya
+                  :direccion,                           # ya
+                  :area_disponible,                     # ya
+                  :area_disponible_desc,      # texto
+                  :valor_mt2,                           # ya
+                  :valor_total,                         # ya
+                  :planos,
+                  :valor_admin,                         # ya
+                  :predial,
+                  :frente,
+                  :fondo,
+                  :otras_caracteristicas,     # texto
+                  :entorno,                   # texto
+                  :info_adicional,
+                  :fecha_disponibilidad,
+                  :fecha_salida_inventario,
+                  :anio_construccion,
+                  :normas_de_uso,             # texto
+                  :indice_ocupacion,
+                  :indice_construccion,
+                  :nro_pisos,
+                  :parqueadores,
+                  :densidad,
+                  :nombre_agente,
+                  :quien_refiere,
+                  :telefono,
+                  :celular,
+                  :email,
+                  :propietario_id,
+                  :porcentaje_comision,
+                  :valor_inmueble,
+                  :altura,
+                  :fecha_captado,
+                  :transporte,                # texto
+                  :nro_pisos_permitidos,
+                  :created_at,
+                  :updated_at,
+                  :nombre_interesado,
+                  :telefono_interesado,
+                  :celular_interesado,
+                  :email_interesado,
+                  :observaciones_interesado,  # texto
+                  :documentos_attributes
 
   validates :codigo_inmueble, :presence => true 
 end
