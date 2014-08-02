9b3e734e0aebd0accae2c782a848696e0bee60c9
Mateo Vidal <jvidalba@gmail.com>
2014-08-02 18:53:36 -0500

    creating new users

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,5 +1,6 @@
 class UsuariosController < ApplicationController
 
+  before_filter :authenticate_user!, :except => [:new, :create]
   def index
     @users = User.all
     respond_to do |format|
@@ -8,6 +9,22 @@ class UsuariosController < ApplicationController
       format.pdf do
         render :pdf => "file"
       end
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      flash[:notice] = "Usuario creado exitosamente"
+      redirect_to usuarios_path
+    else
+      flash[:alert] = "Error creando usuario, por favor verifique los datos ingresados"
+      render :action => 'new'
     end
   end
 
