b6dbb311b663ee94c314cfae12565e4849c1093e
Mateo Vidal <jvidalba@gmail.com>
2015-05-25 23:19:20 -0500

    Fix column documento and name with city nil

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150526031623) do
+ActiveRecord::Schema.define(:version => 20150523222647) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(:version => 20150526031623) do
 
   create_table "documentos", :force => true do |t|
     t.string   "descripcion"
-    t.string   "file_name"
+    t.string   "documento"
     t.integer  "inmueble_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
