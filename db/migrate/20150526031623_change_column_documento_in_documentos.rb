ee607774d190cd5239bad9adee87714cf104e4cf
Mateo Vidal <jvidalba@gmail.com>
2015-05-26 22:08:01 -0500

    Destroy unnecesary migration

--- db/migrate/20150526031623_change_column_documento_in_documentos.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnDocumentoInDocumentos < ActiveRecord::Migration
-  def up
-    rename_column :documentos, :documento, :file_name
-  end
-
-  def down
-    rename_column :documentos, :file_name, :documento
-  end
-end
